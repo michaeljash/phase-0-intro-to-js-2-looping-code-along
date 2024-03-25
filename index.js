a74652cf9ae95f6e7cde59c57f2579328689aaee
jashonmichael <jashon.michael@student.moringaschool.com>
2024-03-25 13:46:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function countDown(startingNumber) {
+    // Iterate from the startingNumber down to 0
+    for (let i = startingNumber; i >= 0; i--) {
+        // Log the current number
+        console.log(i);
+    }
+}
+
+// Example usage:
+countDown(10);
